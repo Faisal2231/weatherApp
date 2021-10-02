0b77d0746c9206fb8cb1b7743d01e0b6b1eab789
Faisal2231 <79972672+Faisal2231@users.noreply.github.com>
2021-10-02 17:32:20 +0530

    Add files via upload

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,1 +1,14 @@
+import React from "react";
 
+import Weather from "./Components/Weather";
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Weather />
+      </div>
+    </div>
+  );
+};
+
+export default App;
